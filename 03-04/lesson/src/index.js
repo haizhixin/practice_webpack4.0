d699b089610394d25253df8433fdcfe7a5ffa831
zhangyaqiang <1126917181@qq.com>
2021-01-25 09:31:01 +0800

    webpack4.0

--- 03-04/lesson/src/index.js
+++ 03-04/lesson/src/index.js
@@ -1,16 +1,16 @@
 // import a from 'a';
 // import b from 'b';
-import react from 'react-dom';
+// import react from 'react-dom';
 
 import { ff } from './test.js';
 console.log(ff());
 
-import _ from 'lodash';
+// import _ from 'lodash';
 
 
-var element = document.createElement('div');
-element.innerHTML = _.join(['Dell', 'Lee'], '-');
-document.body.appendChild(element);
+// var element = document.createElement('div');
+// element.innerHTML = _.join(['Dell', 'Lee'], '-');
+// document.body.appendChild(element);
 
 // function getComponent() {
 // 	return import(/* webpackChunkName:"lodash" */ 'lodash').then(({ default: _ }) => {
